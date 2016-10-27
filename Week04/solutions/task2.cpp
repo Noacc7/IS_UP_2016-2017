b42299eb9444f638e3298ea7ae96c19f93fe06b1
Stoian <stoian_ivanov_ivanov@abv.bg>
2016-10-27 13:57:33 +0300

    add task2 solution

---
+++ Week04/solutions/task2.cpp
@@ -0,0 +1,19 @@
+// Example program
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+    int n = 0;
+    cout << "N=";
+    cin >> n;
+
+    int sum = 0;
+
+    for( int i = 10; i <= n; i++)
+    {
+        sum += (i/10 + i %10);
+    }
+    cout<< "Sum  =" << sum;
+}
