f4ed7fa3def9eedc25130d7d53bcc5d815f7020a
Stoian <stoian_ivanov_ivanov@abv.bg>
2016-10-27 13:43:07 +0300

    solutions for task 1

---
+++ Week04/solutions/task1.cpp
@@ -0,0 +1,25 @@
+// Example program
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+    int number;
+    int sum = 0 ;
+    int counter = 0;
+    while (counter < 5)
+    {
+        cout<< "Enter Number";
+        cin>> number;
+        
+        if( number >= 10  && number <= 5555)
+        {
+            counter++;
+            sum += number; //sum = sum + number 
+        }
+        cout << "Counter:" << counter;
+    }
+    cout << "Sum = "<< sum;
+    //cout<< sum;
+}
